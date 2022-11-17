78c11311946d9cbbb9a42e558c824fc8adadaa10
LiniiiOn <hilkovich380@gmail.com>
2022-11-17 20:54:54 +0300

    chore: add authors to raedme.md

--- startpage/JS/transition.js
+++ startpage/JS/transition.js
@@ -1,3 +1,3 @@
 setTimeout(function(){
-    window.location.href = 'file:///C:/Microsoft%20VS%20Code/Vladu/homepage/homepage.html';
+    window.location.href = 'homepage/homepage.html';
   }, 21 * 1000);
